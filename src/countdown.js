b121387088082630f3b017778d440a2aa6e2fc1b
joklaiber3 <jonah.klaiber@gmx.de>
2025-10-27 04:30:36 +0100

    Update countdown.js

--- src/countdown.js
+++ src/countdown.js
@@ -29,7 +29,7 @@ var x = setInterval(function () {
 
   if (distance < 0) {
     // If time is up
-    window.location.assign("/game-over.html");
+    window.location.href = "./home.html";
     window.localStorage.removeItem("endDate");
   }
 
